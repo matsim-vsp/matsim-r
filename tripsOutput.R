d4f313c97c6af182e143d03bbaeb40ffd525d2c9
zlukich <soboliev@campus.tu-berlin.de>
2022-05-11 20:10:05 +0200

    installed tidyverse

--- tripsOutput.R
+++ tripsOutput.R
@@ -1,5 +1,6 @@
-library("readr")
+library("tidyverse")
 
-table = read.csv("C:/Users/zlukich/Desktop/RSCRIPTS/matsim-r/hamburg-v3.0-1pct-base.output_trips.csv",sep = ";")
+table = read_csv2("C:/Users/zlukich/Desktop/RSCRIPTS/matsim-r/hamburg-v3.0-1pct-base.output_trips.csv")
 
-table[[1]][1:10]
+table[1]
+
