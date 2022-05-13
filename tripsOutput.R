23a9028ddf43b857757ed9c0dc0587532ffaa014
zlukich <soboliev@campus.tu-berlin.de>
2022-05-13 20:26:43 +0200

    readTripsTable added path validation

--- tripsOutput.R
+++ tripsOutput.R
@@ -1,6 +1,21 @@
 library("tidyverse")
 
-table = read_csv2("C:/Users/zlukich/Desktop/RSCRIPTS/matsim-r/hamburg-v3.0-1pct-base.output_trips.csv")
+#Reading of Output_Trips from directory 
+readTripsTable <- function (pathToMATSimOutputDirectory){
+  #Get the file names, output_trips should be there
+  files = list.files(pathToMATSimOutputDirectory)
+  
+  #output_trips is contained as output_trips.csv.gz
+  if(length(grep("output_trips.csv.gz$",files)) !=0){
+    trips_output_table = read_csv2(files[grep("output_trips.csv.gz$",files)],
+                                   col_types = c('c','d','c','t','t','t','d','d','c','c','c','c','c','c','c','d','d','c','c','d','d','c','c'))
+                                            #person is mostly integer, but contains also chars(see Hamburg 110813 observation)
+                
+  }else{ # if Directory doesn't contain trips_output, then nothing to read
+    return(NULL)
+  }
+  
+  
+}
 
-table[1]
 
