6e183f3dcb917516f8fc2c71d459ac5fcd637ba2
zlukich <soboliev@campus.tu-berlin.de>
2022-05-30 13:53:17 +0200

    first function declaration

---
+++ R/plansOutput.r
@@ -0,0 +1,11 @@
+library("tidyverse")
+library("xml2")
+
+xml <- read_xml("./output/hamburg-v3.0-1pct-base.output_plans.xml")
+
+
+readPlans <- function(pathToMaTSimOutputDirectory){
+  
+  
+  return(-1)
+}
