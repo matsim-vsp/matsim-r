fdb47786fdf74561b3b33bb785ecc0cdd3135965
Jakob Rehmann <jakob.rehmann@gmail.com>
2023-04-14 12:56:03 +0200

    changed read_csv2 to read_delim; decimal point as incorrect

--- R/personsOutput.R
+++ R/personsOutput.R
@@ -31,7 +31,7 @@ readPersonsTable <- function(pathToMATSimOutputDirectory = ".") {
                                          first_act_type = col_character(),
                                        )
     )
-    
+
     persons_output_table <- persons_output_table %>% mutate(
       executed_score = as.double(executed_score),
       first_act_x = as.double(first_act_x),
@@ -41,13 +41,14 @@ readPersonsTable <- function(pathToMATSimOutputDirectory = ".") {
     return(persons_output_table)
   }
   if (grepl("output_persons.csv.gz$", pathToMATSimOutputDirectory) == TRUE) {
-    persons_output_table <- read_csv2(pathToMATSimOutputDirectory,
+    persons_output_table <- read_delim(pathToMATSimOutputDirectory,
                                       col_types = cols(
                                         executed_score = col_character(),
                                         first_act_x = col_character(),
                                         first_act_y = col_character(),
                                         first_act_type = col_character(),
-                                      )
+                                      ),
+                                      delim = ";"
     )
     # person is mostly integer, but contains also chars(see Hamburg 110813 observation)
     # doesn't reads coordinates correctly
@@ -59,18 +60,19 @@ readPersonsTable <- function(pathToMATSimOutputDirectory = ".") {
     attr(persons_output_table,"table_name") <- pathToMATSimOutputDirectory
     return(persons_output_table)
   }
-  
+
   files <- list.files(pathToMATSimOutputDirectory, full.names = TRUE)
   # Read from global/local directory
   # output_persons is contained as output_persons.csv.gz
   if (length(grep("output_persons.csv.gz$", files)) != 0) {
-    persons_output_table <- read_csv2(files[grep("output_persons.csv.gz$", files)],
+    persons_output_table <- read_delim(files[grep("output_persons.csv.gz$", files)],
                                       col_types = cols(
                                         executed_score = col_character(),
                                         first_act_x = col_character(),
                                         first_act_y = col_character(),
                                         first_act_type = col_character(),
-                                      )
+                                      ),
+                                      delim = ";"
     )
     # person is mostly integer, but contains also chars(see Hamburg 110813 observation)
     # doesn't reads coordinates correctly
@@ -95,12 +97,12 @@ readPersonsTable <- function(pathToMATSimOutputDirectory = ".") {
 #'
 #' @export
 boxplotScoreDifferences <- function(personTibble_base, personTibble_policy){
-  joined <- inner_join(personTibble_base, personTibble_policy, by = "person", suffix = c("_base", "_policy")) %>% 
+  joined <- inner_join(personTibble_base, personTibble_policy, by = "person", suffix = c("_base", "_policy")) %>%
     select(person,
            executed_score_base,
-           executed_score_policy) %>% 
+           executed_score_policy) %>%
     mutate(score_diff = executed_score_policy - executed_score_base)
-  
+
   result <- ggplot(joined, aes(y = score_diff)) +
     geom_boxplot(fill = "#0099f8") +
     labs(
