23c5c85fa704451cebba57d46b8b058f51bbdf4d
Sasha Soboliev <soboliev@campus.tu-berlin.de>
2023-06-14 14:57:14 +0200

    plot_spatialtype_by_shape_piechart and changed read_config to list output

--- R/config.R
+++ R/config.R
@@ -14,27 +14,28 @@ read_config <- function(input_path = ".", n_max = Inf) {
                           param = character(),
                           value = character())
 
-  config_xml = read_xml(input_path)
+  config_xml<-read_xml(input_path)
 
-  module_nodeset = xml_children(config_xml)
+  module_nodeset<-xml_children(config_xml)
+
+  result_list <- list()
 
   for(i in 1:length(module_nodeset)){
     module_name = xml_attr(module_nodeset[[i]],"name")
-    print(module_name)
+
     param_nodeset = xml_children(module_nodeset[[i]])
     for(j in 1:length(param_nodeset)){
-      param_name = xml_attr(param_nodeset[[j]],"name")
-      value_name = xml_attr(param_nodeset[[j]],"value")
+      param_name<-xml_attr(param_nodeset[[j]],"name")
+      value_name<-xml_attr(param_nodeset[[j]],"value")
       #print(param_name,value_name)
-      temp_tibble = tibble(module = module_name,
-                           param = param_name,
-                           value = value_name
-      )
-      result_tibble = rbind(result_tibble,temp_tibble)
+
+      result_list[[module_name]][[param_name]] <- value_name
+
+
     }
 
   }
 
-  return(result_tibble)
+  return(result_list)
 
 }
