d7a849e638bdcd8e069032077f4e2aaaa51a688f
Johanna Bänsch <j.baensch@campus.tu-berlin.de>
2023-09-11 11:38:35 +0200

    documentation and updated some functions/parameters to follow naming conventions

--- R/output_persons.R
+++ R/output_persons.R
@@ -1,6 +1,7 @@
 matsimDumpOutputDirectory <- "./matsim_r_output"
 dashboard_file <- "/dashboard-1-trips.yaml"
 
+##### DEPRECATED #####
 
 #' Deprecated Function
 #'
@@ -59,15 +60,60 @@ readPersonsTable <- function(input_path = ".",  n_max = Inf) {
   return(persons_output_table)
 }
 
+#' Deprecated Function
+#'
+#' \strong{boxplotScoreDifferences} - function generates a boxplot to compare the score differences between two sets of data
+#' represented by personTibble_base and personTibble_policy tibbles.
+#' It provides insights into the distribution of score differences between the two sets of data.
+#'
+#' @rdname matsimr-deprecated
+#'
+#' @param personTibble_base persons tibble of the base case, can be loaded with readPersonsTable.
+#' @param personTibble_policy persons tibble of the policy case, can be loaded with readPersonsTable.
+#'
+#' @return \strong{boxplotScoreDifferences} - ggplot boxplot of the distribution of the score differences
+#'
+#' @export
+boxplotScoreDifferences <- function(personTibble_base, personTibble_policy){
+
+  joined <- inner_join(personTibble_base, personTibble_policy, by = "person", suffix = c("_base", "_policy")) %>%
+    select(person,
+           executed_score_base,
+           executed_score_policy) %>%
+    mutate(score_diff = executed_score_policy - executed_score_base)
+
+  result <- ggplot(joined, aes(y = score_diff)) +
+    geom_boxplot(fill = "#0099f8") +
+    labs(
+      title = "Distribution of score differences",
+      subtitle = "score_delta = score(policy) - score(base)",
+      #caption = "Source: MTCars dataset",
+      y = "score_delta"
+    ) +
+    theme_classic() +
+    theme(
+      plot.title = element_text(color = "#0099f8", size = 16, face = "bold", hjust = 0.5),
+      plot.subtitle = element_text(face = "bold.italic", hjust = 0.5),
+      plot.caption = element_text(face = "italic"),
+      axis.ticks.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.text.x = element_blank()
+    )
+  result
+}
+
+
+
+
+##### READING #####
+
 #' Load MATSim output_persons table into memory
 #'
-#' Loads a MATSim CSV output_persons from file or archive,
-#' creating a tibble with columns as in csv file
-#' copied + adopted code from readTripsTable in tripsOutput.R
+#' Loads a MATSim CSV output_persons from file or archive creating a tibble.
 #'
-#' @param input_path is a character string, path to the local MATSim output directory, to the persons csv directly, or a http link to the file.
-#' @param n_max integer, maximum number of lines to read within output_persons
-
+#' @param input_path character string, path to the MATSim output directory or http link to the file
+#' @param n_max optional, integer, maximum number of lines to read, standard value is Inf
+#'
 #' @return tibble of output_persons
 #'
 #' @export
@@ -112,64 +158,23 @@ read_output_persons <- function(input_path = ".",  n_max = Inf) {
 
   return(persons_output_table)
 }
-#' Deprecated Function
-#'
-#' \strong{boxplotScoreDifferences} - function generates a boxplot to compare the score differences between two sets of data
-#' represented by personTibble_base and personTibble_policy tibbles.
-#' It provides insights into the distribution of score differences between the two sets of data.
-#'
-#' @rdname matsimr-deprecated
-#'
-#' @param personTibble_base persons tibble of the base case, can be loaded with readPersonsTable.
-#' @param personTibble_policy persons tibble of the policy case, can be loaded with readPersonsTable.
-#'
-#' @return \strong{boxplotScoreDifferences} - ggplot boxplot of the distribution of the score differences
-#'
-#' @export
-boxplotScoreDifferences <- function(personTibble_base, personTibble_policy){
 
-  joined <- inner_join(personTibble_base, personTibble_policy, by = "person", suffix = c("_base", "_policy")) %>%
-    select(person,
-           executed_score_base,
-           executed_score_policy) %>%
-    mutate(score_diff = executed_score_policy - executed_score_base)
+##### COMPARING #####
 
-  result <- ggplot(joined, aes(y = score_diff)) +
-    geom_boxplot(fill = "#0099f8") +
-    labs(
-      title = "Distribution of score differences",
-      subtitle = "score_delta = score(policy) - score(base)",
-      #caption = "Source: MTCars dataset",
-      y = "score_delta"
-    ) +
-    theme_classic() +
-    theme(
-      plot.title = element_text(color = "#0099f8", size = 16, face = "bold", hjust = 0.5),
-      plot.subtitle = element_text(face = "bold.italic", hjust = 0.5),
-      plot.caption = element_text(face = "italic"),
-      axis.ticks.x = element_blank(),
-      axis.title.x = element_blank(),
-      axis.text.x = element_blank()
-    )
-  result
-}
-
-
-#' Boxplot of Score Differences
+#' Create a boxplot of score differences
 #'
-#' The \code{boxplotScoreDifferences} function generates a boxplot to compare the score differences between two sets of data
-#' represented by personTibble_base and personTibble_policy tibbles.
-#' It provides insights into the distribution of score differences between the two sets of data.
+#' Takes two tibbles created by \link{read_output_persons}
+#' to calculate the score difference and create a boxplot.
 #'
-#' @param personTibble_base persons tibble of the base case, can be loaded with readPersonsTable.
-#' @param personTibble_policy persons tibble of the policy case, can be loaded with readPersonsTable.
+#' @param person_tibble1 persons tibble of the base case, can be loaded with read_output_persons.
+#' @param person_tibble2 persons tibble of the policy case, can be loaded with read_output_persons.
 #'
 #' @return ggplot boxplot of the distribution of the score differences
 #'
 #' @export
-plot_compare_score_boxplot <- function(personTibble_base, personTibble_policy){
+plot_compare_score_boxplot <- function(person_tibble1, person_tibble2){
 
-  joined <- inner_join(personTibble_base, personTibble_policy, by = "person", suffix = c("_base", "_policy")) %>%
+  joined <- inner_join(person_tibble1, person_tibble2, by = "person", suffix = c("_base", "_policy")) %>%
     select(person,
            executed_score_base,
            executed_score_policy) %>%
@@ -194,3 +199,4 @@ plot_compare_score_boxplot <- function(personTibble_base, personTibble_policy){
     )
   result
 }
+
